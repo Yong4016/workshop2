2f291a56aad208499a229f21fb3ad18406b6cb70
Yong <yongnah0108@gmail.com>
2023-07-29 01:20:55 +1000

    modify wordfreq.py

--- wordfreq.py
+++ wordfreq.py
@@ -3,7 +3,10 @@
 import sys
 import string
 
-words = sys.stdin.read().split()
+# words = sys.stdin.read().split()
+fhand = open("test.txt")
+words = fhand.read().split()
+
 d = dict()
 for word in words:
 	t = word.translate(str.maketrans('', '', string.punctuation)).upper()
@@ -17,3 +20,4 @@ for key, value in d.items():
 l = sorted(l, reverse=True)
 for v, k in l:
 	print(k, v)
+
